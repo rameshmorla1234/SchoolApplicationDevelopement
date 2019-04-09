66d32df0884878c1e24bd1e0670e161cd71e1713
Ramesh Morla <rameshbabu.ias@gmail.com>
2019-04-09 17:45:37 +0530

    First Commit

---
+++ EducationalOrganization/EODAL/Context/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using EODAL.Models;
+namespace EODAL.Context
+{
+  public  class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=GLT028\\SQLEXPRESS;database=EducationalOrganization.Database;user=sa;password=Ram@2019;", p => p.MigrationsAssembly("GateWay"));
+        }
+    }
+}
---
+++ EducationalOrganization/EODAL/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EODAL.Models
+{
+   public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+    }
+}
